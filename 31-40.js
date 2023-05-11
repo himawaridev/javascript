4896d0ca4d51ff90a821e47d3bdf3d58f07f2fb1
dunghoang22102003 <104152321+dunghoang22102003@users.noreply.github.com>
2023-05-11 16:13:37 +0700

    upodate

--- 31-40.js
+++ 31-40.js
@@ -27,8 +27,41 @@ console.log('cau 32: ', calculateDiv2(16));
 function calculateDiv3(number) {
     let sum = Math.sqrt(Math.floor(2))
     for (let i = 0; i <= number; i++) {
-        Math.sqrt(2 + Math.sqrt(i));
-        console.log(i);
+        sum = Math.sqrt(2 + sum);
     }
+    console.log('cau 33: ', sum);
 }
 calculateDiv3(3);
+
+
+// bai 34: Tính S(n) = CanBac2(n+CanBac2(n – 1 + CanBac2( n – 2 + … + CanBac2(2 + CanBac2(1)  có n dấu căn
+
+function calculateDiv4(n) {
+    if (n === 1) {
+        return 1;
+    } else {
+        return Math.sqrt(n + calculateDiv4(n - 1));
+    }
+}
+
+console.log(calculateS(5));
+
+// bai 35 Tính S(n) = CanBac2(n! + CanBac2((n-1)! +CanBac2((n – 2)! + … + CanBac2(2!) + CanBac2(1!)))) có n dấu căn
+
+function calculateDiv5(n) {
+    if (n === 1 || n === 0) {
+        return 1;
+    } else {
+        return n * calculateDiv5(n - 1);
+    }
+}
+
+function calculateS(n) {
+    if (n === 1) {
+        return 1;
+    } else {
+        return Math.sqrt(calculateDiv5(n) + calculateS(n - 1));
+    }
+}
+
+console.log(calculateS(5));
