efc9e48623bf7ae51304426af2c058ed9f325ec3
dunghoang <dunghoang22102003@gmail.com>
2023-05-19 11:28:54 +0700

    update

--- 71-80.js
+++ 71-80.js
@@ -5,7 +5,7 @@ function question1(x, n) {
     let count = 1;
     let factorial = 1;
     for (let i = 0; i <= n; i++) {
-        factorial *= (2 * i + 1);
+        factorial *= (2 * i + 1)    ;
         count = Math.pow(-1, i + 1) * (Math.pow(x, 2 * i + 1)) / factorial;
         sum += count;
     }
